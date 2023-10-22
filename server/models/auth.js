07dc1c378a5c9f7f72150bea1f526f11802b69c3
Sonam82 <119428557+Sonam82@users.noreply.github.com>
2023-10-22 18:18:27 +0530

    New Changes

--- server/models/auth.js
+++ server/models/auth.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
+  picture: { type: String },
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
