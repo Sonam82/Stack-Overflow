db5b913bd41836816b977e6d9df9b946bc18d309
Sonam82 <119428557+Sonam82@users.noreply.github.com>
2023-10-25 19:13:27 +0530

    New Changes

--- client/src/Pages/About/VedioPlayer.jsx
+++ client/src/Pages/About/VedioPlayer.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 
-import { useGesture } from "@use-gesture/react";
+// import { useGesture } from "@use-gesture/react";
 
 import ReactPlayer from "react-player";
 import video from "../../assets/Intro.mp4";
@@ -12,10 +12,38 @@ const VideoPlayer = () => {
   //   onSwipeRight: () =>
   //     videoRef.current.seekTo(videoRef.current.getCurrentTime() + 10),
   // });
+  const [doubleTapCount, setDoubleTapCount] = React.useState(0);
+
+  const handleMouseDown = (e) => {
+    if (
+      e.nativeEvent.offsetX >
+      videoRef.current.getInternalPlayer().videoWidth / 2
+    ) {
+      // If the tap is on the right side of the video
+      setDoubleTapCount(doubleTapCount + 1);
+      setTimeout(() => {
+        setDoubleTapCount(0);
+      }, 300); // Reset the count after 300 milliseconds
+    }
+  };
+
+  const handleMouseUp = () => {
+    if (doubleTapCount === 2) {
+      // If two taps were detected, move the video forward by 10 seconds
+      const currentTime = videoRef.current.getCurrentTime();
+      videoRef.current.seekTo(currentTime + 10);
+    }
+  };
 
   return (
     <div>
-      <ReactPlayer ref={videoRef} url={video} controls />
+      <ReactPlayer
+        ref={videoRef}
+        url={video}
+        controls
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+      />
     </div>
   );
 };
