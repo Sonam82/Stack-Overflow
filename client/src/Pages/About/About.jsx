55102febc4367ca16471cbd85626d6fa69cfbd3a
Sonam Rathour <119428557+Sonam82@users.noreply.github.com>
2023-12-20 22:06:54 +0530

    Update About.jsx

--- client/src/Pages/About/About.jsx
+++ client/src/Pages/About/About.jsx
@@ -6,12 +6,7 @@ const About = () => {
     <div className="home-container-1">
       <LeftSidebar />
       <div className="home-container-2">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero corporis
-        consectetur accusantium nihil ullam qui neque, sunt excepturi dicta hic
-        provident ex amet nam esse molestiae minima numquam eius incidunt. Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Harum inventore non
-        veniam ullam, optio voluptatibus minus eos porro nemo totam distinctio
-        maiores animi libero perferendis! Commodi nulla sint fugit placeat.
+        Here is a video, which gives some information about this Project...
         <VideoPlayer />
       </div>
     </div>
