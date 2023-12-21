05bd82ad0d259a3890f3fb3b8c96d94876392ac7
Sonam Rathour <119428557+Sonam82@users.noreply.github.com>
2023-12-21 08:52:26 +0530

    Update VedioPlayer.jsx

--- client/src/Pages/About/VedioPlayer.jsx
+++ client/src/Pages/About/VedioPlayer.jsx
@@ -8,55 +8,54 @@ import video from "../../assets/Intro.mp4";
 const VideoPlayer = () => {
   const videoRef = useRef(null);
 
-  // const [doubleScreenCount, setDoubleScreenCount] = useState(0);
+  const [doubleScreenCount, setDoubleScreenCount] = useState(0);
 
-  // const handleScreenHold = (progress) => {
-  //   if (doubleScreenCount === 2) {
-  //     const currentTime = videoRef.current.getCurrentTime();
-  //     const changement = currentTime + progress;
-  //     videoRef.current.seekTo(changement);
-  //     console.log(`i am active ${currentTime} now changed by ${changement}`);
-  //   }
-  // };
+  const handleScreenHold = () => {
+    if (doubleScreenCount === 2) {
+      const currentTime = videoRef.current.getCurrentTime();
+      const changement = currentTime + progress;
+      videoRef.current.seekTo(changement);
+      // console.log(`i am active ${currentTime} now changed by ${changement}`);
+    }
+  };
 
-  // const handleTouchStart = (ev) => {
-  //   // ev.stopImmediatePropagation();
-  //   let screenx = ev.changedTouches[0]["screenX"];
+  const handleTouchStart = (ev) => {
+    // ev.stopImmediatePropagation();
+    let screenx = ev.changedTouches[0]["screenX"];
 
-  //   let MidWidth = videoRef.current.offsetWidth / 2;
+    let MidWidth = videoRef.current.getInternalPlayer().videoWidth / 2;
 
-  //   if (screenx > MidWidth) {
-  //     setDoubleScreenCount((doubleScreenCount) => doubleScreenCount + 1);
+    if (screenx > MidWidth) {
+      setDoubleScreenCount((doubleScreenCount) => doubleScreenCount + 1);
 
-  //     handleScreenHold(0);
+      handleScreenHold(0);
 
-  //     setTimeout(() => {
-  //       setDoubleScreenCount(0);
-  //     }, 3000);
+      setTimeout(() => {
+        setDoubleScreenCount(0);
+      }, 3000);
 
-  //     console.log(
-  //       `${screenx} Right Side ${MidWidth}, now u clicked me by ${doubleScreenCount}`
-  //     );
-  //   } else {
-  //     setDoubleScreenCount((doubleScreenCount) => doubleScreenCount + 1);
+      console.log(
+        `${screenx} Right Side ${MidWidth}, now u clicked me by ${doubleScreenCount}`
+      );
+    } else {
+      setDoubleScreenCount((doubleScreenCount) => doubleScreenCount + 1);
 
-  //     handleScreenHold(-5);
+      handleScreenHold(-5);
 
-  //     setTimeout(() => {
-  //       setDoubleScreenCount(0);
-  //     }, 3000);
+      setTimeout(() => {
+        setDoubleScreenCount(0);
+      }, 3000);
 
-  //     console.log(
-  //       `${screenx} Left Side ${MidWidth}, now u clicked me by ${doubleScreenCount}`
-  //     );
-  //   }
-  // };
+      console.log(
+        `${screenx} Left Side ${MidWidth}, now u clicked me by ${doubleScreenCount}`
+      );
+    }
+  };
 
   return (
     <div>
       <ReactPlayer
-        // light
-        // onTouchStart={(e) => handleTouchStart(e)}
+         onTouchStart={(e) => handleTouchStart(e)}
         // onTouchEnd={handleTouchEnd}
         ref={videoRef}
         url={video}
